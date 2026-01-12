346d43aecd01fbc1de8d91aa1ab35414baa8901e
zakiyul <zakiyul.py@gmail.com>
2026-01-12 21:33:31 +0800

    gtw lupa gw

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export const NavbarWrapper = () => {
     return (
         <>
-            <div className="flex gap-4 my-2">
+            <div className="bg-cyan-400 flex gap-4 my-2">
                 <Link href='/'>Home</Link>
                 <Link href='/kopi'>List</Link>
                 <Link href='/about'>About</Link>
