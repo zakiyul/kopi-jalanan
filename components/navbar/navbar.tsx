1965c3ddbe000e75920a5ac09f307434c2fb7c7b
zakiyul <zakiyul.py@gmail.com>
2026-01-15 22:08:37 +0800

    add humberger btn for mobile

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 
 export const NavbarWrapper = () => {
     return (
+        // NAVBAR 
         <nav className="fixed w-full z-50 top-0 left-0 bg-cyan-200 backdrop-blur-md border-b border-cyan-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
@@ -15,15 +16,15 @@ export const NavbarWrapper = () => {
                         <Link href={'/about'} className="hover:text-cyan-400 transition-colors">About</Link>
                         <Link href={'/kopi'} className="hover:text-cyan-400 transition-colors">Kopi</Link>
                     </div>
+
+                    {/* MOBILE */}
+                    <div className="md:hidden">
+                        <button className="text-gray-300 hover:text-white focus:outline-none">
+                            BTN
+                        </button>
+                    </div>
                 </div>
             </div>
-            {/* <div className="bg-cyan-400 flex gap-4 my-2">
-                <Link href='/'>Home</Link>
-                <Link href='/kopi'>List</Link>
-                <Link href='/about'>About</Link>
-            </div>
-            <hr /> */}
-
         </nav>
     )
 }
